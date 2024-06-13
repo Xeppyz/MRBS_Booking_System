4d00fe85c72a075a21af7ce368457f88cfb742c0
Xeppyz <alexander.xampos10@gmail.com>
2024-06-13 10:13:06 -0600

    I have many things to do

---
+++ docker_app/php/config.inc.php
@@ -0,0 +1,11 @@
+<?php // -*-mode: PHP; coding:utf-8;-*-
+namespace MRBS;
+
+$timezone = "America/Managua";
+$dbsys = "mysql";
+$db_host = "localhost";
+$db_database = "mrbs";
+$db_login = "root";
+$db_password = "";
+$db_tbl_prefix = "mrbs_";
+$db_persist = FALSE;
